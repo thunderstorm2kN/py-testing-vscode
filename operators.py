ad6d3ee732bfef38e66fee738f7a4f3e3ab5b65a
Cristian <https://github.com/thunderstorm2kN?tab=repositories>
2026-09-15 09:28:15 +0300

    updating

--- operators.py
+++ operators.py
@@ -10,9 +10,14 @@ print("This is subtraction w/ -", subtraction)
 
 #Multiplications: Made with asterisk sign * - ctrl 8 
 multiplication= 5*5
-print("This is multimpllication w/ *", multiplication)
+print("This is multiplication w/ *", multiplication)
 
 #Division: Made with slash /
 division = 10 / 5
 print("This is division", division)
+
+#Double division aka floor division: Made with double slash // 
+#In order to get the whole number of a division, we use double slash //
+double_division = 10 // 3
+print("This is double division", double_division)   
 
