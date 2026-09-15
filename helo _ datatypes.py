2a655d36eaa481bc0a476558f5fdf4d21e77ea48
Cristian <https://github.com/thunderstorm2kN?tab=repositories>
2026-09-15 09:22:20 +0300

    Initial commit

---
+++ helo _ datatypes.py
@@ -0,0 +1,39 @@
+#a few basic instructions appear in just about every language
+
+# A few basic instructions appear in almost every language.
+
+# 1. Output: display something in the terminal
+print("Hello from VS Code!")
+
+
+# 2. Input: get data from the keyboard
+name = input("What is your name? ")
+print("Hello, " + name)
+
+
+# 3. Math: perform calculations
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+product = number1 * number2
+
+print("The sum is:", sum_result)
+print("The product is:", product)
+
+
+# 4. Conditional execution: make a decision
+age = int(input("How old are you? "))
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are under 18.")
+
+
+# 5. Repetition: repeat an action
+for count in range(5):
+    print("This is repetition number", count + 1)
+
+
+#Book: think py Think Python, 2nd Edition -- Allen B_ Downey
---
+++ operators.py
@@ -0,0 +1,18 @@
+#Aritmetic operators
+
+#Addition : Made with + sign
+addition = 6+1
+print("This is the addition w/ + :" , addition)
+
+#Subtraction: Made with - sign
+subtraction = 5-1
+print("This is subtraction w/ -", subtraction)
+
+#Multiplications: Made with asterisk sign * - ctrl 8 
+multiplication= 5*5
+print("This is multimpllication w/ *", multiplication)
+
+#Division: Made with slash /
+division = 10 / 5
+print("This is division", division)
+
