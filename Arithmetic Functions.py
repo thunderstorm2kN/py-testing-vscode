62d3e76b0f6bf8b19cdbed0c3aafd8a3e20308cc
Cristian~ <https://github.com/thunderstorm2kN?tab=repositories>
2026-09-17 11:53:52 +0300

    upd

--- Arithmetic Functions.py
+++ Arithmetic Functions.py
@@ -1,4 +1,8 @@
 #A multitude of numbers + collections is called expression. 
-#Expressions examples with mulple nrs and Operators:
-print(2 + 3 * 4) # 14
+#Example of expressions  with mulple numbers and Operators:
+print(2 + 3 ** 4) # 14
+
+#Python uses same order of operations as mathematics.
+#But with parentheses, you can change the order of operations.
+print((12 + 5) * 6) 
 
